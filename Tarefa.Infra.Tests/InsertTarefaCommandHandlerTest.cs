1b0e189e3a1f3291583454680707cf8792471c66
Bruno Silva <brunpersilva@gmail.com>
2021-05-25 21:21:41 -0300

    Added unit test for TarefaCommandHandler

---
+++ Tarefa.Infra.Tests/InsertTarefaCommandHandlerTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Tarefas.Infra.AppConfigurations;
+using Tarefas.Infra.Tarefa.Command;
+
+namespace Tarefas.Infra.Tests
+{
+    public class InsertTarefaCommandHandlerTest
+    {
+        [Test]
+        public void InsertTarefaShouldWork()
+        {
+            AppConfiguration configuration = new AppConfiguration { ConnectionString = "Data Source=localhost;Initial Catalog=Tarefas_Banco;Integrated Security=True" };
+
+            var command = new InsertTarefaCommand
+            {
+                Titulo = "Tarefa Teste",
+                Descricao = "Descrição teste",
+            };
+            var handler = new InsertTarefaCommandHandler(configuration);
+
+            var result = handler.Handle(command, CancellationToken.None);
+
+            Assert.AreEqual(command.Titulo, result.Result.Titulo);
+            Assert.AreEqual(command.Descricao, result.Result.Descricao);
+        }
+    }
+}
